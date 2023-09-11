2bcc23896f817d883b56e5332d3d26601f04debb
unknown <pelcastrevargasz@gmil.com>
2023-09-11 15:46:36 -0600

    04 Operadores

---
+++ operadores.js
@@ -0,0 +1,34 @@
+// Operadores Matematicos 
+// + - * / % **
+// += -= *= /= %= **=
+console.log(10 + 10);
+console.log(10 - 10);
+console.log(10 / 10);
+console.log(10 * 10);
+console.log(10 % 10);
+console.log(10 ** 10); // Potencia 
+
+let num = 15;
+console.log(num += 5); // num = num + 5 
+console.log(num -= 5);
+console.log(num *= 5);
+console.log(num /= 5);
+console.log(num %= 5);
+
+
+// operadores booleanos 
+// < > >= <= !== ===
+console.log(1>2);
+console.log(1<2);
+console.log(1>=2);
+console.log(1<=2);
+console.log(1!==2);
+console.log(1===2);
+
+// Operadores logicos 
+// and &&
+console.log(true && true);
+// or || 
+console.log(true || false);
+// not !
+console.log(!true);
